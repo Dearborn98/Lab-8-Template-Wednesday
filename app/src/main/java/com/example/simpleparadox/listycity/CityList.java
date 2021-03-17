354887ba8d97ec24308da705bbe9c5860d0badfd
Andrew <adearbor@ualberta.ca>
2021-03-16 18:15:00 -0600

    test passes

--- app/src/main/java/com/example/simpleparadox/listycity/CityList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CityList.java
@@ -77,4 +77,9 @@ public class CityList {
     public int countCities() {
         return cities.size();
     }
+
+    public City getCity(String name) {
+        City city = new City(name, "selector");
+        return cities.get(cities.indexOf(city));
+    }
 }
